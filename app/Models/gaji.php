f9796464de97fe8e3452937237ea3a2cf9bf7268
abdlmnf17 <11210195@bsi.ac.id>
2024-05-19 12:41:19 +0700

    Memperbaiki Gaji

--- app/Models/gaji.php
+++ app/Models/gaji.php
@@ -8,21 +8,34 @@ use Illuminate\Database\Eloquent\Model;
 class Gaji extends Model
 {
     use HasFactory;
-    protected $table = 'gaji';
-    protected $fillable = [
 
-        'kd_gaji',
-        'tgl_gaji',
-        'jam_mengajar',
-        'id_tunjangan',
-        'id_potongan',
-        'id_guru',
-        'gaji_pokok',
-        'sub_total',
+    protected $fillable = [
+        'guru_id', 'gaji_pokok', 'kd_gaji',
+        'tgl_gaji', 'jam_mengajar', 'sub_total',
     ];
 
-    public static function factory()
+    public function tunjangan()
     {
-        return new GajiFactory();
+        return $this->belongsToMany(Tunjangan::class, 'detail_gaji_tunjangan', 'gaji_id', 'tunjangan_id');
+    }
+
+    public function karyawan()
+    {
+        return $this->belongsTo(Karyawan::class);
+    }
+
+    public function potongan()
+    {
+        return $this->belongsToMany(Potongan::class, 'detail_gaji_potongan', 'gaji_id', 'potongan_id');
+    }
+
+    public function honorMengajar()
+    {
+        return $this->belongsTo(HonorMengajar::class);
+    }
+
+    public function jurnals()
+    {
+        return $this->hasMany(Jurnal::class);
     }
 }
