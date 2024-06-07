4107511db2a1e05be486357441f806b96e737047
unknown <sriyuliani037@gmail.com>
2024-06-08 02:09:13 +0700

    mengubah hak akses

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -95,8 +95,7 @@
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Data Transaksi</h6>
                         <a class="collapse-item" href="/gaji">Penggajian</a>
-                        <a class="collapse-item" href="/jurnal/create">Jurnal Umum</a>
-
+                       
                     </div>
                 </div>
             </li>
